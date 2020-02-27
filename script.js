dca544272158d12eb7b19ee053af88454ad23d43
The Farrier <nkfarrey@gmail.com>
2020-02-26 20:54:40 -0600

    Created search value query

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=ahLm9LQTqmWk22OWGsU3RnzDz48QEVFc";
+var search = $("searchbox").val
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=ahLm9LQTqmWk22OWGsU3RnzDz48QEVFc";
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
 
-    //
-      .then(function(response) {
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
 
-        console.log(response);
-      });
-// a comment
+//
+  .then(function(response) {
+
+    console.log(response);
+  });
+
+
+
