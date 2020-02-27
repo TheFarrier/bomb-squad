e8a76ef77b6755ac53c0bf384d0712be9e3e4361
The Farrier <nkfarrey@gmail.com>
2020-02-26 21:45:46 -0600

    everything works except clicking the link

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-
-var search = $("searchbox").val
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=ahLm9LQTqmWk22OWGsU3RnzDz48QEVFc";
+var srchBtn = $("#run-search");
 
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-})
 
-//
-  .then(function(response) {
+function getArticles(event){
+  var search = $("#search-term").val();
+  var count = $("#article-count").val();
+  var start = $("#start-year").val();
+  var end = $("#end-year").val();
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=ahLm9LQTqmWk22OWGsU3RnzDz48QEVFc";
 
-    console.log(response);
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+
+    for(i=0; i<=count; i++ ){
+      displayArticles(response);
+    }
+
   });
+}
 
+function displayArticles(response){
+  var main = $("#article-section");
+  var article = $("<div>").addClass("card");
+  var title = $("<div>").addClass("card-header");
+  var story = $("<div>").addClass("card-body");
 
+  title.text(response.response.docs[i].headline.main);
+  story.text(response.response.docs[i].abstract);
+  console.log(title);
+  article.click(function(event){
+    event.stopPropagation()
+    window.location.replace(response.response.docs[i].web_url);
+  })
 
+  article.append(title, story);
+  main.append(article);
+}
+
+srchBtn.click(function(){
+  event.preventDefault();
+  getArticles();
+})
